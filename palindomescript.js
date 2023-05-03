514f3be5296980bc443796ab96be4fe3b2c6964d
mahajanmuskaan <er.muskaan21@gmail.com>
2023-05-03 18:07:01 +0530

    Edge Cases handled.

--- palindomescript.js
+++ palindomescript.js
@@ -3,12 +3,20 @@ const messageElement = document.getElementById("message");
 
 myButton.addEventListener("click", function () {
     const enteredText = document.getElementById("formGroupExampleInput").value;
-    const string = enteredText.toString();
-    if (string == string.split('').reverse().join('')) {
-        console.log(string + ' is palindrome.');
-        messageElement.textContent = string + ' is Palindrome.';
+    if (enteredText.length == 0) {
+        messageElement.textContent = "Please enter the Text/Number first.";
+        messageElement.style.color = "red";
     }
     else {
-        messageElement.textContent = string + ' is NOT a Palindrome.';
+        const string = enteredText.toString();
+        if (string == string.split('').reverse().join('')) {
+            console.log(string + ' is palindrome.');
+            messageElement.textContent = string + ' is Palindrome!';
+            messageElement.style.color = "green";
+        }
+        else {
+            messageElement.textContent = string + ' is NOT a Palindrome!';
+            messageElement.style.color = "#2A2F4F";
+        }
     }
 });
